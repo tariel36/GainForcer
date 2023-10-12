44b876427adcc000df6e9b8a4e1f6ea47abde231
tariel36 <2964246+tariel36@users.noreply.github.com>
2023-10-12 18:29:14 +0900

    forces gain control to 100%

---
+++ GainForcer/GainForcer/Program.cs
@@ -0,0 +1,49 @@
+﻿using NAudio.Mixer;
+
+UnsignedMixerControl volumeControl = null;
+
+while (volumeControl == null)
+{
+    try
+    {
+        int waveInDeviceNumber = 0;
+        var mixerLine = new MixerLine((IntPtr) waveInDeviceNumber,
+                                       0, MixerFlags.WaveIn);
+
+        foreach (var control in mixerLine.Controls)
+        {
+            if (control.ControlType == MixerControlType.Volume)
+            {
+                volumeControl = control as UnsignedMixerControl;
+                break;
+            }
+        }
+    }
+    catch
+    {
+        // Ignored
+    }
+
+    if (volumeControl == null)
+    {
+        await Task.Delay(1000);
+    }
+    else
+    {
+        while (true)
+        {
+            try
+            {
+                volumeControl.Percent = 100;
+
+                await Task.Delay(10);
+            }
+            catch
+            {
+                volumeControl = null;
+                break;
+            }
+        }
+    }
+}
+
